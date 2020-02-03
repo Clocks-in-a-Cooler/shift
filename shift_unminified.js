8d986b900ec1743d997f4b0bfa8fe210321aa6ce
clocks-in-a-cooler <34520271+Clocks-in-a-cooler@users.noreply.github.com>
2020-02-02 21:42:45 -0500

    added a shifting red dot controlled by the shift keys.

--- shift_unminified.js
+++ shift_unminified.js
@@ -36,8 +36,30 @@ var player = {
     colour: "firebrick",
     x: window.innerWidth / 2, y: 500,
     radius: 4,
+    jump_length: 10, jump_variation: 40,
+    jumped_left: false, jumped_right: false,
     update: function(lapse) {
+        /*
+        this.x += this.speed * lapse * (keys.left ? -1 : 0);
+        this.x += this.speed * lapse * (keys.right ? 1 : 0);
+        */
+        if (!this.jumped_left && keys.left) {
+            this.jumped_left = true;
+            console.log("jumped left");
+            //jump to the left
+            this.x -= this.jump_length + Math.random() * this.jump_variation;
+        }
         
+        if (!this.jumped_right && keys.right) {
+            this.jumped_right = true;
+            console.log("jumped right");
+            this.x += this.jump_length + Math.random() * this.jump_variation; 
+        }
+        
+        this.jumped_left = this.jumped_left && keys.left;
+        this.jumped_right = this.jumped_right && keys.right;
+        
+        this.x = Math.max(this.radius, Math.min(this.x, canvas.width - this.radius));
     },
     
     reset: function() {
@@ -60,7 +82,25 @@ Obstacle.prototype.update = function(lapse) {
     
 };
 
+function draw() {
+    cxt.clearRect(0, 0, canvas.width, canvas.height);
+    obstacles.forEach(o => {
+        cxt.fillStyle = o.colour;
+        cxt.beginPath();
+        cxt.arc(o.x, o.y, o.radius, 0, Math.PI * 2);
+        cxt.closePath();
+        cxt.fill();
+    })
+    
+    cxt.fillStyle = player.colour;
+    cxt.beginPath();
+    cxt.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
+    cxt.closePath();
+    cxt.fill();
+}
+
 var last_time = null, lapse = 0;
+var playing = false;
 function animate(time) {
     if (last_time == null) {
         lapse = 0;
@@ -69,9 +109,22 @@ function animate(time) {
     }
     last_time = time;
     
-    requestAnimationFrame(animate);
+    if (playing) {
+        player.update(lapse);
+        obstacles.forEach(o => { o.update(lapse); });
+        requestAnimationFrame(animate);
+    }
+    
+    draw();
 }
 
-function start() {
-    requestAnimationFrame(animate);
+function start(evt) {
+    if (evt.key == "Shift") {
+        document.getElementById("instructions").style.visibility = "hidden";
+        playing = true;
+        requestAnimationFrame(animate);
+        removeEventListener("keyup", start);
+    }
 }
+
+addEventListener("keyup", start);
